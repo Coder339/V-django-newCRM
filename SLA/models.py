6836385f162949d19f854410d17d1258806b39df
AmanpreetKaurLeanvia <amanpreet.leanvia@outlook.com>
2020-04-02 19:59:58 +0530

    fields modification done

--- SLA/models.py
+++ SLA/models.py
@@ -6,15 +6,16 @@ from authentication.models import *
 priority=(('HIGH','High'),
           ('LOW','Low'),
           ('MODERATE','Moderate'))
-stage=(('IN PROGRESS','In Progress'),
+stage=(('INITIATED','Initiated'),('IN PROGRESS','In Progress'),
        ('RESOLVED','Resolved'))
 
 class SLA(models.Model):
-    customer_details         =              models.ForeignKey(Customer,on_delete=models.CASCADE,default='',null=True)
+    customer_details        =               models.ForeignKey(Customer,on_delete=models.CASCADE,default='',null=True)
     ticket_no               =               models.IntegerField()
-    issue_details           =               models.TextField(blank='Flase', null='False')
+    problem_details         =               models.TextField(blank='False', null='False')
     priority                =               models.CharField(choices=priority,max_length=200)
-    employee_details        =               models.ForeignKey(EmployeeProfile,on_delete=models.CASCADE,default='')
+    date                    =               models.DateField(default='')
+    responsible_person      =               models.ForeignKey(EmployeeProfile,on_delete=models.CASCADE,default='')
     status                  =               models.CharField(choices=stage,max_length=200,null=True)
    
     class Meta():
